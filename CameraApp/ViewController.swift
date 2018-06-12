1f2a3d458d3fbab4c3868fd5af1756d6bdd26b8b
Nobuhiro Takahashi <nt@feb19.jp>
2018-06-13 03:53:01 +0900

    update

--- CameraApp/ViewController.swift
+++ CameraApp/ViewController.swift
@@ -12,26 +12,79 @@ import AVFoundation
 class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
 
     @IBOutlet weak var previewView: UIView!
+    @IBOutlet weak var shutterButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
     var session = AVCaptureSession()
     var output = AVCapturePhotoOutput()
     let notification = NotificationCenter.default
+    var authorizeStatus: AuthorizeStatus = .authorized
+    var inOutStatus: InputOutputStatus = .ready
+    var shareImage: UIImage?
+    
+    enum AuthorizeStatus {
+        case authorized
+        case notAuthorized
+        case failed
+    }
+    enum InputOutputStatus {
+        case ready
+        case notReady
+        case failed
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        shutterButton.isEnabled = false
+        shareButton.isEnabled = false
         
-        if session.isRunning {
+        guard !session.isRunning else {
             return
         }
-        
+        auth()
         setup()
-        preview()
         
-        session.startRunning()
+        if (authorizeStatus == .authorized) && (inOutStatus == .ready) {
+            preview()
+            session.startRunning()
+            shutterButton.isEnabled = true
+        } else {
+            showAlert(appName: "カメラ")
+        }
         notification.addObserver(self,
                                  selector: #selector(self.deviceOrientationDidChanged(_:)),
                                  name: NSNotification.Name.UIDeviceOrientationDidChange,
                                  object: nil)
+    }
+    
+    func auth() {
+        let status = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
+        switch status {
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: AVMediaType.video, completionHandler: { [unowned self] authorized in
+                print("This print is first time. \(authorized.description)")
+                if authorized {
+                    self.authorizeStatus = .authorized
+                } else {
+                    self.authorizeStatus = .notAuthorized
+                }
+            })
+        case .restricted, .denied:
+            authorizeStatus = .notAuthorized
+        case .authorized:
+            authorizeStatus = .authorized
+        }
+    }
+    
+    func showAlert(appName :String) {
+        let alert = UIAlertController(title: "\(appName)のプライバシー設定",
+            message: "設定→プライバシー→\(appName)の利用を許可してください。",
+            preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        alert.addAction(UIAlertAction(title: "設定を開く", style: UIAlertActionStyle.default, handler: { (action) in
+            UIApplication.shared.open(URL(string: UIApplicationOpenSettingsURLString)!, options: [:], completionHandler: nil)
+        }))
+        self.present(alert, animated: true, completion: nil)
     }
     
     @objc func deviceOrientationDidChanged(_ notification: Notification) {
@@ -91,11 +144,15 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
     }
 
     @IBAction func shotButtonWasTapped(_ sender: Any) {
-        let setting = AVCapturePhotoSettings()
-        setting.flashMode = .auto
-        setting.isAutoStillImageStabilizationEnabled = true // 自動補正？
-        setting.isHighResolutionPhotoEnabled = false
-        output.capturePhoto(with: setting, delegate: self)
+        if (authorizeStatus == .authorized) && (inOutStatus == .ready) {
+            let setting = AVCapturePhotoSettings()
+            setting.flashMode = .auto
+            setting.isAutoStillImageStabilizationEnabled = true // 自動補正？
+            setting.isHighResolutionPhotoEnabled = false
+            output.capturePhoto(with: setting, delegate: self)
+        } else {
+            showAlert(appName: "カメラ")
+        }
     }
     
     func photoOutput(_ output: AVCapturePhotoOutput,
@@ -107,7 +164,20 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
         
         if let stillImage = UIImage(data: photoData) {
             UIImageWriteToSavedPhotosAlbum(stillImage, self, nil, nil)
+            shareImage = stillImage
+            shareButton.isEnabled = true
         }
+    }
+    
+    @IBAction func actionButtonWasTapped(_ sender: UIBarButtonItem) {
+        guard let shareImage = shareImage else {
+            return
+        }
+        let shareText = "これをシェアします"
+        let shareUrl = ""
+        let activities = [shareText, shareUrl, shareImage] as [Any]
+        let vc = UIActivityViewController(activityItems: activities, applicationActivities: nil)
+        self.present(vc, animated: true, completion: nil)
     }
 }
 
